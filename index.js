f75e962940b4293f32bb8322401bbfbfec1650ee
Katherine Tsai <katherinetsai@DESKTOP-10SDEV1.localdomain>
2022-11-01 20:51:02 -0400

    commit tree 6dcf1485edecb77cf2dcc3725d1fbb6a75034a83

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  let greeting
+  if (feet <= 400) {
+    greeting = 'This one is on me!'
+  }
+  else if (feet < 2000) {
+    greeting = 'That will be twenty bucks.'
+  }
+  else if (feet < 2500) {
+    greeting = 'I will gladly take your thirty bucks.'
+  }
+  else {
+    greeting = 'No can do.'
+  }
+  return greeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  const isNyc = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return isNyc;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
